e4d0fdba3b9804f74ac9033b90d1dd9a9875c9ff
Anisa Thomas <anisathomas99@gmail.com>
2018-06-28 19:29:52 +0000

    Now able to show restaurants on react app page.Done by adding a get request in server.js to make an API call to yelp

--- client/src/App.js
+++ client/src/App.js
@@ -9,18 +9,19 @@ class App extends Component {
 
   componentDidMount() {
     this.callApi()
-      .then(res => this.setState({ response: res.express }))
+      .then(res => this.setState({ response: res}))
       .catch(err => console.log(err));
   }
 
   callApi = async () => {
-    const response = await fetch('/api/hello');
+    const response = await fetch('/api/search');
     const body = await response.json();
 
     if (response.status !== 200) throw Error(body.message);
 
     return body;
   };
+
   render() {
     return (
       <div className="App">
--- server.js
+++ server.js
@@ -1,12 +1,38 @@
 const express = require('express');
+const yelp = require('yelp-fusion');
 
 const app = express();
 const port = process.env.PORT || 5000;
 
 app.get('/api/hello', (req, res) => {
-  res.send({ express: 'Hello From Express' });
+  res.send({ message: 'Hello From Express' });
 });
+
+app.get('/api/search', (req, res) => {
+  client.search({
+    offset: 5,
+    categories: 'bubbletea',
+    location: 'toronto, on'
+  }).then(response => {
+    const businesseNames = response.jsonBody.businesses
+    res.send(getName(businesseNames));
+  }).catch(e => {
+    console.log(e);
+  });
+
+});
+
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
 
+
+const client = yelp.client('sqO1jfckyHBvxwS4GaxCdOKVR8CPGAF9dmjXsSUNQCNmw7hXoxez-AiGrLsjI2KNAIlAxjbRqb_ZuiIzb-8fAzcZakqeLooDK_FRAr81Vx7HeiDBkDTvwF9ydtUzW3Yx');
+
+function getName(array) {
+  var businessArray = []
+  for(var i = 0; i < array.length; i++){
+    businessArray.push(array[i].name);
+  }
+  return businessArray;
+}
 
